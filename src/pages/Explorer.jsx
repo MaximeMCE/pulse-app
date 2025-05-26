55f1672f24dae8042108191839ac29b6bb3382a9
Maxime Chanteux <43016908-maxime111@users.noreply.replit.com>
2025-05-26 17:51:21 +0000

    🐛 Fix oversized artist images in Explorer

--- src/pages/Explorer.jsx
+++ src/pages/Explorer.jsx
@@ -1,4 +1,4 @@
-// Updated Explorer.jsx with search inspiration tiles
+// Updated Explorer.jsx with search inspiration tiles and fixed image size
 import React, { useState, useEffect } from 'react';
 import { searchArtists } from '../api/Spotify';
 
@@ -178,7 +178,7 @@ const Explorer = () => {
                   <img
                     src={artist.images[0].url}
                     alt={artist.name}
-                    className="w-[80px] h-[80px] rounded-full mr-4 object-cover"
+                    className="w-20 h-20 rounded-full mr-4 object-cover"
                   />
                 )}
                 <div className="flex-1">
